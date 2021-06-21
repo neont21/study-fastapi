fbb27636c8984d1273b80dcd8ee4ae775dbb8020
Pt J <peter.j@kakao.com>
2021-06-22 07:05:47 +0900

    Tutorial

---
+++ tutorial2/main.py
@@ -0,0 +1,25 @@
+import time
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.get('/')
+async def root():
+    return {'message': 'Hello World'}
+
+@app.middleware('http')
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers['X-Process-Time'] = str(process_time)
+    return response
